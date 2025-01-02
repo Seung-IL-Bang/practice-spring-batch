e35020214a20991da971046891df39cd45178c88
Seungil <seungilbang@khu.ac.kr>
2025-01-02 21:54:33 +0900

    section04.2 JobInstance

---
+++ src/main/java/com/practice/springbatch/JobInstanceConfiguration.java
@@ -0,0 +1,62 @@
+package com.practice.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class JobInstanceConfiguration {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    public JobInstanceConfiguration(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    JobInstance jobInstance = contribution.getStepExecution().getJobExecution().getJobInstance();
+                    System.out.println("JobInstance ID: " + jobInstance.getInstanceId());
+                    System.out.println("JobInstance Instance ID: " + jobInstance.getInstanceId());
+                    System.out.println("JobInstance Name: " + jobInstance.getJobName());
+                    System.out.println("JobInstance Version: " + jobInstance.getVersion());
+                    System.out.println("Step1 FINISHED");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    JobInstance jobInstance = contribution.getStepExecution().getJobExecution().getJobInstance();
+                    System.out.println("JobInstance ID: " + jobInstance.getInstanceId());
+                    System.out.println("JobInstance Instance ID: " + jobInstance.getInstanceId());
+                    System.out.println("JobInstance Name: " + jobInstance.getJobName());
+                    System.out.println("JobInstance Version: " + jobInstance.getVersion());
+                    System.out.println("Step2 FINISHED");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}

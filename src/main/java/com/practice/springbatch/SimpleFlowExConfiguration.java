4352f7583c3456bd24f7f1a8cc244e9c3228f370
Seungil <seungilbang@khu.ac.kr>
2025-01-12 23:04:00 +0900

    section08.9 SimpleFlow 개념 및 API

---
+++ src/main/java/com/practice/springbatch/SimpleFlowExConfiguration.java
@@ -0,0 +1,124 @@
+package com.practice.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class SimpleFlowExConfiguration {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    public SimpleFlowExConfiguration(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
+
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(flow1())
+                .on("COMPLETED").to(flow2())
+                .from(flow1())
+                .on("FAILED").to(flow3())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Flow flow1() {
+        return new FlowBuilder<Flow>("flow1")
+                .start(step1())
+                .next(step2())
+                .end();
+    }
+
+    @Bean
+    public Flow flow2() {
+        return new FlowBuilder<Flow>("flow2")
+                .start(flow3())
+                .next(step5())
+                .next(step6())
+                .end();
+    }
+
+    @Bean
+    public Flow flow3() {
+        return new FlowBuilder<Flow>("flow3")
+                .start(step3())
+                .next(step4())
+                .end();
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+//                    throw new RuntimeException("step1 failed");
+                    System.out.println("step1 was executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 was executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return new StepBuilder("step3", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return new StepBuilder("step4", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    throw new RuntimeException("step4 failed");
+//                    System.out.println("step4 was executed");
+//                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return new StepBuilder("step5", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+//                    throw new RuntimeException("step5 failed");
+                    System.out.println("step5 was executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step6() {
+        return new StepBuilder("step6", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step6 was executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}

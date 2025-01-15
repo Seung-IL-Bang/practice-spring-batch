37eaceafc866a80ca334248321788f7876a54dbc
Seungil <seungilbang@khu.ac.kr>
2025-01-15 22:51:17 +0900

    section10.2 FlatFileItemReader - DelimitedLineTokenizer

--- src/main/java/com/practice/springbatch/FlatFileDelimitedConfiguration.java
+++ src/main/java/com/practice/springbatch/FlatFileDelimitedConfiguration.java
@@ -3,12 +3,15 @@ package com.practice.springbatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -17,12 +20,12 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-public class FlatFileConfiguration {
+public class FlatFileDelimitedConfiguration {
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
 
-    public FlatFileConfiguration(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public FlatFileDelimitedConfiguration(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         this.jobRepository = jobRepository;
         this.transactionManager = transactionManager;
     }
@@ -30,6 +33,7 @@ public class FlatFileConfiguration {
     @Bean
     public Job job() {
         return new JobBuilder("job", jobRepository)
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build();
@@ -39,7 +43,7 @@ public class FlatFileConfiguration {
     public Step step1() {
         return new StepBuilder("step1", jobRepository)
                 .<Customer, Customer>chunk(3, transactionManager)
-                .reader(itemReader())
+                .reader(itemReader2())
                 .writer(new ItemWriter<Customer>() {
                     @Override
                     public void write(Chunk<? extends Customer> chunk) throws Exception {
@@ -63,15 +67,26 @@ public class FlatFileConfiguration {
 
     @Bean
     public ItemReader<Customer> itemReader() {
-        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
-        itemReader.setResource(new ClassPathResource("customers.csv"));
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("customerItemReader")
+                .resource(new ClassPathResource("customers.csv"))
+                .fieldSetMapper(new CustomerFieldSetMapper())
+                .delimited().delimiter(",")
+                .names(new String[]{"name", "age", "year"})
+                .linesToSkip(1)
+                .build();
+    }
 
-        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
-        lineMapper.setTokenizer(new DelimitedLineTokenizer());
-        lineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
-
-        itemReader.setLineMapper(lineMapper);
-        itemReader.setLinesToSkip(1);
-        return itemReader;
+    @Bean
+    public ItemReader<Customer> itemReader2() {
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("customerItemReader")
+                .resource(new ClassPathResource("customers.csv"))
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
+                .targetType(Customer.class)
+                .delimited().delimiter(",")
+                .names(new String[]{"name", "age", "year"})
+                .linesToSkip(1)
+                .build();
     }
 }

66c4619441f05b351dad93cf9b3976edd94e899d
Seungil <seungilbang@khu.ac.kr>
2025-01-16 22:34:29 +0900

    section10.6 DB - JdbcCursorItemReader

---
+++ src/main/java/com/practice/springbatch/Customer.java
@@ -0,0 +1,34 @@
+package com.practice.springbatch;
+
+public class Customer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate='" + birthdate + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/practice/springbatch/JdbcCursorConfiguration.java
@@ -0,0 +1,75 @@
+package com.practice.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcCursorConfiguration {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final DataSource dataSource;
+
+    public JdbcCursorConfiguration(JobRepository jobRepository, PlatformTransactionManager transactionManager, DataSource dataSource) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    public Step step() {
+        return new StepBuilder("step", jobRepository)
+                .<Customer, Customer>chunk(10, transactionManager)
+                .reader(customItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<Customer> customItemReader() {
+        return new JdbcCursorItemReaderBuilder<Customer>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .fetchSize(10) // chunk size와 동일하게 설정
+                .sql("SELECT id, firstName, lastName, birthdate" +
+                        " FROM customer" +
+                        " WHERE firstName" +
+                        " LIKE ? ORDER BY lastName, firstName")
+                .beanRowMapper(Customer.class) // setter 를 이용한 매핑
+                .queryArguments("A%")
+                .maxItemCount(3)
+                .currentItemCount(0)
+                .maxRows(100)
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<Customer> customItemWriter() {
+        return chunk -> {
+            for (Customer customer : chunk.getItems()) {
+                System.out.println(customer);
+            }
+        };
+    }
+
+}
